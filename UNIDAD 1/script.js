617e9394ee41fa48af132cac98ab0d4c8f33bdb1
Fran Vergara <vergarafran12345@gmail.com>
2025-09-17 12:07:42 +0200

    primer commit

---
+++ UNIDAD 1/script.js
@@ -0,0 +1,12 @@
+function pintar_ruta() {
+    console.log("El acceso a la ruta C:\\usuario\\ tarda 1'23 segundos");
+}
+function saludar() {
+var edad = prompt("Por favor, introduce tu edad:");
+            if (edad <=18) {
+                alert("Eres menor de edad.");
+            } else {
+                alert("Eres mayor de edad.");
+            }
+            alert("Tu edad es: " + edad);
+}
