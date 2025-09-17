39f93a57f0cf5fc1ee98b31f783feadded588004
Fran Vergara <vergarafran12345@gmail.com>
2025-09-17 12:38:31 +0200

    resolvemos ejercicio 3 y 4

--- UNIDAD 1/script.js
+++ UNIDAD 1/script.js
@@ -1,6 +1,13 @@
 function pintar_ruta() {
     console.log("El acceso a la ruta C:\\usuario\\ tarda 1'23 segundos");
 }
+
+function pintar_ruta2() {
+    var substring = "El acceso a la ruta C:\\\\usuario\\tarda 1'23 segundos\, algo";
+    var substring2 = "considerado \"lento\" en la actualidad.";
+    console.log(substring + " " + substring2);
+}
+
 function saludar() {
 var edad = prompt("Por favor, introduce tu edad:");
             if (edad <=18) {
@@ -9,4 +16,9 @@ var edad = prompt("Por favor, introduce tu edad:");
                 alert("Eres mayor de edad.");
             }
             alert("Tu edad es: " + edad);
+}
+
+function muestra_potencia() {
+    var num= 2* Math.pow(10, -9);
+    console.log("el resultado es" + num);
 }
